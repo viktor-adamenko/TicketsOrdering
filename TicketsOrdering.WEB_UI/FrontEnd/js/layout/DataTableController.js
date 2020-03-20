47b9cbeba75405edc95e65cb88e79edb64d2fad9
Адаменко Виктор Сергеевич <viktor.adamenko@beeper.ru>
2020-03-20 13:21:50 +0200

    added ua localization

--- TicketsOrdering.WEB_UI/FrontEnd/js/layout/DataTableController.js
+++ TicketsOrdering.WEB_UI/FrontEnd/js/layout/DataTableController.js
@@ -1,3 +1,26 @@
+let uaLocalization = {
+    "processing":   "Зачекайте...",
+	"search":       "Пошук:",
+    "lengthMenu":   "Показати _MENU_ записів",
+    "info":         "Записи з _START_ по _END_ із _TOTAL_ записів",
+    "infoEmpty":    "Записи з 0 по 0 із 0 записів",
+    "infoFiltered": "(відфільтровано з _MAX_ записів)",
+    "infoPostFix":  "",
+    "loadingRecords":       "Завантаження записів...",
+	"zeroRecords": "Записи відсутні.",
+	"emptyTable":   "В таблиці записи відсутнію",
+    "paginate": {
+        "first": "Перша",
+        "previous": "Попередня",
+        "next": "Наступна",
+        "last": "Остання"
+    },
+    "aria": {
+        "sortAscending":  ": активувати для сортування стовпців за зростанням",
+        "sortDescending": ": активувати для сортування стовпців за спаданням"
+    }
+};
+
 class DataTableController {
 
     create(config) {
@@ -5,6 +28,7 @@ class DataTableController {
         let table = $(config.selector).DataTable({
             ordering: false,
             searching: false,
+            language: uaLocalization,
             pageLength: 6,
             columns: config.columns,
             ajax: config.ajax
